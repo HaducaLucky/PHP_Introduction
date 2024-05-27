13e47e0af30660b5f65ff8c90972799807240f64
HaducaLucky <147017329+HaducaLucky@users.noreply.github.com>
2024-05-27 19:14:35 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    echo"I love Adobo <br>";
+    echo"Sobrang Sarap";
+    //? Echo = System.out.print
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Linkx</title>
+</head>
+<body>
+    
+    <br>
+    <button>Order Abobo</button>
+
+</body>
+</html>
